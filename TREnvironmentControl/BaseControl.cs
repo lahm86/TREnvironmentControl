a2737744c73838c6e1d36573bf7a52f6fa9b5728
lahm86 <33758420+lahm86@users.noreply.github.com>
2023-02-11 11:24:51 +0000

    Base structure
    Resolves #1.

---
+++ TREnvironmentControl/BaseControl.cs
@@ -0,0 +1,35 @@
+﻿using TREnvironmentEditor;
+using TRRandomizerCore.Secrets;
+
+namespace TREnvironmentControl
+{
+    public abstract class BaseControl<E> : BaseIO
+        where E : class
+    {
+        public abstract string Level { get; }
+        public abstract string MappingPath { get; }
+        public abstract string SecretRoomPath { get; }
+        public abstract void GenerateEnvironmentMapping();
+        public virtual void GenerateSecretRoomMapping() { }
+
+        protected EMEditorMapping GetMapping()
+        {
+            return EMEditorMapping.Get(MappingPath);
+        }
+
+        protected void WriteMapping(EMEditorMapping mapping)
+        {
+            mapping.SerializeTo(MappingPath);
+        }
+
+        protected TRSecretMapping<E> GetSecretRoomMapping()
+        {
+            return TRSecretMapping<E>.Get(SecretRoomPath);
+        }
+
+        protected void WriteSecretRoomMapping(TRSecretMapping<E> mapping)
+        {
+            mapping.SerializeTo(SecretRoomPath);
+        }
+    }
+}
